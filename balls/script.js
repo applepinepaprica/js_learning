94d5e79ae83b7a7bc4a3a1fb9faad13fa15de621
applepinepaprica <applepinepaprica@gmail.com>
2018-09-14 17:39:54 +0300

    Adding event handler

--- balls/script.js
+++ balls/script.js
@@ -1,44 +1,55 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
-var balls = [];
+let balls = [];
 
-canvas.addEventListener('click', newBall, false);
+canvas.addEventListener('click', test, false);
 ctx.fillStyle = 'rgba(0,0,0)';
 ctx.fillRect(0,0,width,height);
 
 function random(min,max) {
-    var num = Math.floor(Math.random()*(max-min)) + min;
-    return num;
+  let num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
 }
 
 function Ball(x, y, color, size) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
-    this.size = size;
+  this.x = x;
+  this.y = y;
+  this.color = color;
+  this.size = size;
 }
 
 Ball.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
 };
 
+function test(event){
+  let x = event.pageX;
+  let y = event.pageY;
+  let element = balls[balls.length - 1];
+  if (Math.sqrt((x-element.x)*(x-element.x) + (y-element.y)*(y-element.y)) < element.size) {
+    newBall();
+  } else {
+    alert('Game over!');
+  }
+}
+
 function newBall() {
-    var size = random(10,20);
-    var ball = new Ball(
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-      size
-    );
-    balls.push(ball);
-    ball.draw();
+  let size = random(10,20);
+  let ball = new Ball(
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+    size
+  );
+  balls.push(ball);
+  ball.draw();
 }
 
 newBall();
